ba3c41343275182844938f0a216f5697438c010d
Wojtek Henszke <Henszke.wojtek@gmail.com>
2019-08-10 14:11:33 +0200

    Created Singleton for connection

--- src/main/java/pl/sda/jdbc/App.java
+++ src/main/java/pl/sda/jdbc/App.java
@@ -1,13 +1,65 @@
 package pl.sda.jdbc;
 
+import java.sql.*;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    static{
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public App() {
+
+    }
+
+    public static void main(String[] args ) throws SQLException {
+        Connection connection = DbConnector.getConnection();
+
+
+        Statement statement =  null;
+
+
+        try {
+
+            statement = connection.createStatement();
+            String insert = "insert  into " +
+                    "`employees`(`employeeNumber`,`lastName`,`firstName`,`extension`,`email`,`officeCode`,`reportsTo`,`jobTitle`) " +
+                    "values (?,?,?,?,?,?,?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(insert);
+            preparedStatement.setInt(1,1999);
+            preparedStatement.setString(2,"Karolczak");
+            preparedStatement.setString(3,"Mariusz");
+            preparedStatement.setString(4,"exteee");
+            preparedStatement.setString(5,"email@gg.com");
+            preparedStatement.setInt(6,6);
+            preparedStatement.setInt(7,1002);
+            preparedStatement.setString(8,"jobbing");
+            preparedStatement.execute();
+            String sql = "select * from employees";
+            ResultSet resultSet = statement.executeQuery(sql);
+            System.out.println(resultSet);
+            while (resultSet.next()){
+                System.out.println("Imie i Nazwisko "+resultSet.getString("firstName")+ " " +
+                         resultSet.getString("lastName")+
+                        " Email: "+resultSet.getString("email"));
+            }
+
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            connection.close();
+        }
     }
 }
---
+++ src/main/java/pl/sda/jdbc/DbConnector.java
@@ -0,0 +1,51 @@
+package pl.sda.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnector {
+
+
+    private static Connection connection;
+    private final static String ADRESS = "jdbc:mysql://localhost";
+    private final static String DATABASE = "employee_db";
+    private final static String USER = "admin";
+    private final static String PASSWORD = "admin";
+    private final static String PORT = "8889";
+    private final static String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private final static String PARAMS = "serverTimezone=UTC";
+
+    private static String getFormatedUrl(){
+        return ADRESS+":"+PORT+"/"+DATABASE+"?"+PARAMS;
+    }
+
+    private static void loadDriver(){
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static void loadConnection(){
+        try {
+            connection = DriverManager.getConnection(getFormatedUrl(),USER,PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection(){
+        if(connection == null){
+            loadDriver();
+            loadConnection();
+        }
+        return connection;
+    }
+
+    private DbConnector() {
+
+    }
+}
